faf2f710d04d34dd313b5f10050c337d24ffbf76
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-18 18:50:04 +0700

    tests(Header.test.js) test content

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,52 +1,146 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { isElementOfType } from 'react-dom/test-utils';
+import { render, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Header from './Header.jsx';
 
-describe('<Header/>', () => {
-	describe('desktop view', () => {
-		describe('on render', () => {
+// ==================================== Consts & vars ==================================== //
+const logoChars = 'ASWD';
+const navLinkNames = ['link-1', 'link-2', 'link-3']; 
+
+let header;
+let textLogo;
+let nav;
+let navLinks;
+let burger;
+
+// ==================================== Utils Fns ==================================== //
+function renderDesktop() {
+	const iframe = document.createElement('iframe');
+	iframe.style.width = '426px'; 
+
+	render(<Header logoChars={logoChars} navLinks={navLinkNames}/>, { wrapper: iframe })
+	getParts()
+}
+
+function renderPhone() {
+	const iframe = document.createElement('iframe');
+	iframe.style.width = '425px'; 
+
+	render(<Header logoChars={logoChars} navLinks={navLinkNames}/>, { wrapper: iframe })
+	getParts()
+}
+
+function getParts() {
+	header = document.querySelector('header');
+	textLogo = document.querySelector('.text-logo');
+	nav = document.querySelector('nav');
+	navLinks = nav.querySelectorAll('a');
+	burger = document.querySelector('.burger');
+}
+
+// ==================================== Teardown ==================================== //
+afterEach(() => {
+	cleanup()
+	console.log('header after Teardown', header)
+})
+
+// ==================================== Tests ======================================= //
+describe('<Header logoChars={""} navLinks={[""]}/>', () => {
+	describe('render', () => {
+		describe('desktop', () => {
 			it('should render', () => {
+				renderDesktop()
 
-			})
-
-			it('should render text logo', () => {
-
-			})
-
-			it('should render nav links', () => {
-
+				expect(isElementOfType(header, Header)).toEqual(true)
 			})
 		})
-		
-		describe('nav links', () => {
-			describe('on hover', () => {
 
+		describe('phone', () => {
+			it('should render', () => {
+				renderPhone()
+
+				expect(isElementOfType(header, Header)).toEqual(true)
 			})
-	 
-			describe('on click', () => {
+		})	
+	})
+	
+	describe('text logo', () => {
+		describe('desktop', () => {
+			it('should render text logo', () => {
+				renderDesktop()
+				expect(textLogo).not.toEqual(null)
+			})
 
+			it('text logo should have characters passed in props.logoChars', () => {
+				renderDesktop()
+				expect(textLogo.textContent).toEqual(logoChars)
+			})
+		})
+
+		describe('phone', () => {
+			it('should render text logo', () => {
+				renderPhone()
+				expect(textLogo).not.toEqual(null)
+			})
+
+			it('text logo should have characters passed in props.logoChars', () => {
+				renderPhone()
+				expect(textLogo.textContent).toEqual(logoChars)
 			})
 		}) 
 	})
 
-	describe('mobile view', () => {
-		describe('on render', () => {
-			it('should render', () => {
+	describe('nav links', () => {
+		describe('desktop', () => {
+			it('should render a nav link for each item from props.navLinks', () => {
+				renderDesktop()
 
-			})
-
-			it('should render text logo', () => {
-
-			})
-
-			it('should render burger', () => {
-
-			})
-		}) 
-
-		describe('on click burger', () => {
-			it('should add ".burger-selected" to burger', () => {
-
+				navLinkNames.forEach((navLinkName, i) => {
+					expect(navLink[i].textContent).toEqual(navLinkName)
+				})
 			})
 		})
+
+		describe('phone', () => {
+			it('should not render nav links', () => {
+				renderPhone()
+
+				expect(nav).toEqual(null)
+			})
+		}) 
+	})
+
+	describe('burger', () => {
+		describe('desktop', () => {
+			it('should not render burger', () => {
+				renderDesktop()
+
+				expect(burger).toEqual(null)
+			})
+		})
+
+		describe('phone', () => {
+			it('should render burger', () => {
+				renderPhone()
+
+				expect(burger).not.toEqual(null)
+			})
+
+			it('should add ".burger-selected" when touched once', () => {
+				renderPhone()
+
+				fireEvent.touch(burger)
+				expect(phone.className).toContain("burger-selected")
+			})
+
+			it('should remove ".burger-selected" when touched twice', () => {
+				renderPhone()
+
+				fireEvent.touch(burger)
+				fireEvent.touch(burger)
+				expect(phone.className).not.toContain("burger-selected")
+			})
+		}) 
 	})
 })
