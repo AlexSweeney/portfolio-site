272793ced97ae468278338828ef8d689c525c8f9
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-18 17:11:56 +0700

    tests(Header.test.js) add test descriptions

---
+++ src/Header/Header.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+describe('<Header/>', () => {
+	describe('desktop view', () => {
+		describe('on render', () => {
+			it('should render', () => {
+
+			})
+
+			it('should render text logo', () => {
+
+			})
+
+			it('should render nav links', () => {
+
+			})
+		})
+		
+		describe('nav links', () => {
+			describe('on hover', () => {
+
+			})
+	 
+			describe('on click', () => {
+
+			})
+		}) 
+	})
+
+	describe('mobile view', () => {
+		describe('on render', () => {
+			it('should render', () => {
+
+			})
+
+			it('should render text logo', () => {
+
+			})
+
+			it('should render burger', () => {
+
+			})
+		}) 
+
+		describe('on click burger', () => {
+			it('should add ".burger-selected" to burger', () => {
+
+			})
+		})
+	})
+})
