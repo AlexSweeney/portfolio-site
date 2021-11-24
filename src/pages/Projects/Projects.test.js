dda8243fc6424ed856be39a5747d5e2d2689b16f
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-24 10:51:37 +0700

    tests - added descriptions

---
+++ src/pages/Projects/Projects.test.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+describe('<Projects projs={[]}/>', () => {
+  describe('desktop', () => {
+    describe('render', () => {
+      it('should render', () => {
+
+      }) 
+    })
+
+    describe('content', () => {
+      it('should render all projects in prop.projs', () => {
+
+      })
+    })
+
+    describe('style', () => {
+      describe('background', () => {
+        it('should have background = colors.background.dark', () => {
+
+        })
+      })
+
+      describe('layout', () => {
+        it(`should have style = {
+          display: flex;
+          flex-direction: column;
+        }`, () => {
+
+        })
+      })
+    })
+  })
+
+  describe('phone', () => {
+    describe('render', () => {
+      it('should render', () => {
+
+      }) 
+    })
+
+    describe('content', () => {
+      it('should render all projects in prop.projs', () => {
+        
+      })
+    })
+
+    describe('style', () => {
+      describe('background', () => {
+        it('should have background = colors.background.dark', () => {
+
+        })
+      })
+
+      describe('layout', () => {
+        it(`should have style = {
+          display: flex;
+          flex-direction: column;
+        }`, () => {
+
+        })
+      })
+    })
+  })
+})
