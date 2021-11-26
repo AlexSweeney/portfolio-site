7803396017c04dc1207d0e2a018c52ac7f2bef5e
Alex Sweeney <alexsweeney@live.co.uk>
2021-11-26 19:37:06 +0700

    tests - fix tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Header from './components/Header/Header.jsx';
+import Home from './pages/Home/Home.jsx';
 
 export default function App() {
+  const logoChars = 'ASWD';
+  const navLinks = ['Home', 'Technical Skills', 'Project', 'Contact'];
+
   return (
-    <section>
-       
+    <section className="app">
+       <Header logoChars={logoChars} navLinks={navLinks}/>
+       <Home/>
     </section>
   )
 }
