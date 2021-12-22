6d86ef381e7fcf02832a90427c4e3d60e6b63f0b
Alex Sweeney <alexsweeney@live.co.uk>
2021-12-22 13:31:48 +0700

    Updates

---
+++ precache-manifest.0fb87706f4689ded58fe651028a6fe39.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae9d834652611ccf2850d20026d4132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5324d9610681d2875d3e",
+    "url": "/static/css/main.1c611033.chunk.css"
+  },
+  {
+    "revision": "d2c96701f83872605218",
+    "url": "/static/js/2.d5835fe3.chunk.js"
+  },
+  {
+    "revision": "bd3bf084b1fa551cf41f4979b00f8f31",
+    "url": "/static/js/2.d5835fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5324d9610681d2875d3e",
+    "url": "/static/js/main.d35200a4.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "ca46dbbeab3472a65f6e7d9817b1f5d7",
+    "url": "/static/media/flex-quest.ca46dbbe.jpg"
+  },
+  {
+    "revision": "d04bb39327bb5d40050930775accc3a8",
+    "url": "/static/media/kairos.d04bb393.png"
+  },
+  {
+    "revision": "fb77a1ef83efdb19eae9f54774066421",
+    "url": "/static/media/metronome.fb77a1ef.jpg"
+  },
+  {
+    "revision": "b77c50264c5fb46bf1f870f37b3badc0",
+    "url": "/static/media/portfolio-site.b77c5026.jpg"
+  },
+  {
+    "revision": "fc7261b99b289d101b2f92afb71c0560",
+    "url": "/static/media/react-piano.fc7261b9.jpg"
+  }
+]);
