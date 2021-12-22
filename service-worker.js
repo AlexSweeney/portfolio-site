99850316781ab3044e82a68f7094da24f007dd5c
Alex Sweeney <alexsweeney@live.co.uk>
2021-12-22 16:23:13 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-site/precache-manifest.6b205b3bd2e3bc15b1892b69c011a637.js"
+  "/portfolio-site/precache-manifest.f43f7ee53c60de740c8f287a79162c91.js"
 );
 
 self.addEventListener('message', (event) => {
